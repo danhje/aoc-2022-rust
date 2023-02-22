3355cd3e66e4a20351320250cd007f8261d9772f
Daniel Hjertholm <daniel.hjertholm@icloud.com>
2023-02-22 20:31:05 +0100

    Second exercise

--- two/src/main.rs
+++ two/src/main.rs
@@ -1,24 +1,38 @@
 use std::fs;
 
-
 fn read_data() -> String {
-  return fs::read_to_string("./data/test_data")
-        .expect("Should have been able to read the file");
+    return fs::read_to_string("./data/data").expect("Should have been able to read the file");
 }
 
+fn calculate_score(round: &str) -> u32 {
+  let hand_shapes = round.split(' ').collect::<Vec<&str>>();
+  let [opponents_move, my_move] = <[&str; 2]>::try_from(hand_shapes).ok().unwrap();
+  
+  let outcome_score = match (opponents_move, my_move) {
+    ("A", "X") => 3,
+    ("A", "Y") => 6,
+    ("A", "Z") => 0,
+    ("B", "X") => 0,
+    ("B", "Y") => 3,
+    ("B", "Z") => 6,
+    ("C", "X") => 6,
+    ("C", "Y") => 0,
+    ("C", "Z") => 3,
+    _ => 0,
+  };
+  
+  let shape_score = match my_move {
+    "X" => 1,
+    "Y" => 2,
+    "Z" => 3,
+    _ => 0,
+  };
 
-fn solve(data: String) -> i32 {
-  for line in data.lines() {
-    if let Some((a, b)) = line.split_once(' ') {
-      println!("{} {}", a, b);
-    }
-  }
-  1;
+  return shape_score + outcome_score;
 }
-
 
 fn main() {
-  let test_data: String = read_data();
-  let ans = solve(test_data);
-  println!("{}", ans);
+  let data = read_data();
+  let total: u32 = data.lines().map(calculate_score).sum();
+  println!("{:?}", total);
 }
